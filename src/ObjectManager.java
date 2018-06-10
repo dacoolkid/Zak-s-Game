31d9eda5dba1431ba35946fca0d3516fa05dacec
League <league@WTS9.attlocal.net>
2018-06-10 15:56:02 -0700

    Yes

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -2,15 +2,18 @@ import java.awt.Graphics;
 
 public class ObjectManager {
 	Hand handy;
-	public ObjectManager(Hand h) {
+	Pen penny;
+	public ObjectManager(Hand h, Pen p) {
 		handy = h;
+		penny = p;
 	}
 	public void update() {
 		handy.update();
+		penny.update();
 	}
 	public void draw(Graphics g) {
 	handy.draw(g);
-	
+	penny.draw(g);
 	
 	}
 	
